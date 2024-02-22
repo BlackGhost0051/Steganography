99510006ae33427cceca61789582316224e33d06
Black Ghost <balckghostgithub@gmail.com>
2024-02-22 12:31:04 +0100

    WSL 01

--- Whitespace Language/main.cpp
+++ Whitespace Language/main.cpp
@@ -1,3 +1,9 @@
+// https://en.wikipedia.org/wiki/Whitespace_(programming_language)
+// https://codernet.ru/articles/drugoe/whitespace_yazyik_programmirovaniya_s_samyim_neobyichnyim_sintaksisom/
+
+// ' ' = 0
+// '\t' = 1
+
 #include <iostream>
 #include <fstream>
 
@@ -5,6 +11,7 @@ using namespace std;
 
 int main() {
     char myChar;
+    string result_butes;
     ifstream MyReadFile;
 
     MyReadFile.open("input.txt");
@@ -17,12 +24,22 @@ int main() {
     while (MyReadFile.get(myChar)) {
         if (myChar == '\n') {
             cout << "\\n";
+            result_butes += '\n';
         } else if (myChar == '\t'){
             cout << "\\t";
+            result_butes += '1';
+        } else if (myChar == ' '){
+            cout << "\\s";
+            result_butes += '0';
         }
         cout << myChar;
     }
 
     MyReadFile.close();
+
+
+
+    cout << "\n\nResult:\n" << result_butes << endl;
+
     return 0;
 }
