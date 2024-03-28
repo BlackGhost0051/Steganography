4f8825fa7c2a4546a463dae0308b9d317b36fc46
Black Ghost <balckghostgithub@gmail.com>
2024-03-28 13:12:35 +0100

    WSL 02

--- Whitespace Language/main.cpp
+++ Whitespace Language/main.cpp
@@ -1,6 +1,6 @@
 // https://en.wikipedia.org/wiki/Whitespace_(programming_language)
 // https://codernet.ru/articles/drugoe/whitespace_yazyik_programmirovaniya_s_samyim_neobyichnyim_sintaksisom/
-
+// https://www.ascii-code.com/
 // ' ' = 0
 // '\t' = 1
 
@@ -9,10 +9,64 @@
 
 using namespace std;
 
+string find_Chars_in_String(string bytes){
+    string result;
+    int i = 0;
+
+    while(true){
+      if(bytes[i] == '\0'){
+        return result;
+      }
+    }
+    return "Error";
+}
+
+
+string getText(string bytes){
+  int i,j, start = 0, end, line_length;
+  string result;
+
+  for(i = 0; i < bytes.length(); i++){
+	if(bytes[i] == '\n'){
+		end = i-1;
+
+		line_length = end - start;
+		if(line_length >= 8){
+
+			cout << "good " << start << " " << end << endl <<  "Line = "<< line_length  << endl;
+			cout << "\n\n START = " << bytes[start]  << endl;
+
+
+			for(j=start; j <= end; j++){
+				cout << bytes[j];
+			}
+
+
+			cout << "\n STOP = " <<  bytes[end] << endl;
+		}
+
+		j=i;
+		while(j != bytes.length() - 1){
+			if(bytes[j] != '\n'){
+				start = j;
+				break;
+			}
+			j++;
+		}
+
+	}
+  }
+
+  return result;
+}
+
+
 int main() {
     char myChar;
     string result_butes;
+    string bytes;
     ifstream MyReadFile;
+
 
     MyReadFile.open("input.txt");
 
@@ -24,13 +78,13 @@ int main() {
     while (MyReadFile.get(myChar)) {
         if (myChar == '\n') {
             cout << "\\n";
-            result_butes += '\n';
+            result_butes += "\n";
         } else if (myChar == '\t'){
             cout << "\\t";
-            result_butes += '1';
+            result_butes += "1";
         } else if (myChar == ' '){
             cout << "\\s";
-            result_butes += '0';
+            result_butes += "0";
         }
         cout << myChar;
     }
@@ -38,8 +92,11 @@ int main() {
     MyReadFile.close();
 
 
-
     cout << "\n\nResult:\n" << result_butes << endl;
+
+    cout << "\n\nResult: "<< getText(result_butes) << endl;
+
+//    find_Chars_in_String("test");
 
     return 0;
 }
