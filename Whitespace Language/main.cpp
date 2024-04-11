b6f022f7ad3cb4a02c9299d1e52f77dff69d6b3a
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-04-11 12:40:52 +0200

    Add byte to char conversion

--- Whitespace Language/main.cpp
+++ Whitespace Language/main.cpp
@@ -1,6 +1,5 @@
-// https://en.wikipedia.org/wiki/Whitespace_(programming_language)
-// https://codernet.ru/articles/drugoe/whitespace_yazyik_programmirovaniya_s_samyim_neobyichnyim_sintaksisom/
-// https://www.ascii-code.com/
+// Made by Black Ghost | 2024
+
 // ' ' = 0
 // '\t' = 1
 
@@ -9,94 +8,76 @@
 
 using namespace std;
 
-string find_Chars_in_String(string bytes){
-    string result;
-    int i = 0;
-
-    while(true){
-      if(bytes[i] == '\0'){
-        return result;
-      }
-    }
-    return "Error";
-}
-
 
 string getText(string bytes){
-  int i,j, start = 0, end, line_length;
+  int i,j,k, start = 0, end, line_length;
   string result;
 
   for(i = 0; i < bytes.length(); i++){
-	if(bytes[i] == '\n'){
-		end = i-1;
+    if(bytes[i] == '\n'){
+      end = i-1;
 
-		line_length = end - start;
-		if(line_length >= 8){
+      line_length = end - start;
+      if(line_length >= 8){
+  
+        char c = 0; //
+        int byte_start = end - 7; // last 8 bits
 
-			cout << "good " << start << " " << end << endl <<  "Line = "<< line_length  << endl;
-			cout << "\n\n START = " << bytes[start]  << endl;
+        for(j=byte_start; j <= end; j++){
+      
+          // cout << bytes[j];
+          c = (c << 1) | (bytes[j] - '0');
+        }
+        // cout << "\nSumbol: " << c << endl;
+        result += c;
+      }
 
+      j=i;
+      while(j != bytes.length() - 1){
+        if(bytes[j] != '\n'){
+          start = j;
+          break;
+        }
+        j++;
+      }
 
-			for(j=start; j <= end; j++){
-				cout << bytes[j];
-			}
-
-
-			cout << "\n STOP = " <<  bytes[end] << endl;
-		}
-
-		j=i;
-		while(j != bytes.length() - 1){
-			if(bytes[j] != '\n'){
-				start = j;
-				break;
-			}
-			j++;
-		}
-
-	}
+    }
   }
-
   return result;
 }
 
 
 int main() {
-    char myChar;
-    string result_butes;
-    string bytes;
-    ifstream MyReadFile;
+  char myChar;
+  string result_butes;
+  string bytes;
+  ifstream MyReadFile;
 
 
-    MyReadFile.open("input.txt");
+  MyReadFile.open("input.txt");
 
-    if (!MyReadFile.is_open()) {
-        cout << "Failed to open the file" << endl;
-        return 1;
+  if (!MyReadFile.is_open()) {
+      cout << "Failed to open the file" << endl;
+      return 1;
+  }
+
+  while (MyReadFile.get(myChar)) {
+    if (myChar == '\n') {
+      result_butes += "\n";
+    } else if (myChar == '\t'){
+      result_butes += "1";
+    } else if (myChar == ' '){
+      result_butes += "0";
     }
+  }
 
-    while (MyReadFile.get(myChar)) {
-        if (myChar == '\n') {
-            cout << "\\n";
-            result_butes += "\n";
-        } else if (myChar == '\t'){
-            cout << "\\t";
-            result_butes += "1";
-        } else if (myChar == ' '){
-            cout << "\\s";
-            result_butes += "0";
-        }
-        cout << myChar;
-    }
-
-    MyReadFile.close();
+  MyReadFile.close();
 
 
-    cout << "\n\nResult:\n" << result_butes << endl;
+  cout << "====  Start input text  ====\n" << result_butes << "====  End input text  ===="<< endl;
 
-    cout << "\n\nResult: "<< getText(result_butes) << endl;
+  cout << "\nResult = " << getText(result_butes) << endl;
 
-//    find_Chars_in_String("test");
 
-    return 0;
+  return 0;
 }
