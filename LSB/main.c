54fb1ec1d1b3ffa683b62e78ccc904a07271dbc7
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-04-25 14:24:14 +0200

    Add check file structure

--- LSB/main.c
+++ LSB/main.c
@@ -1,7 +1,74 @@
-//#include <SDL2/SDL.h>
 #include <stdio.h>
 
 
+
+void checkStructure(char* fileName){
+    int byte, i;
+    unsigned char fileStructure[8];
+    unsigned char pngStructure[] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+    unsigned char gifStructure[] = {0x47, 0x49, 0x46, 0x38, 0x39, 0x61};
+    unsigned char bmpStructure[] = {0x42, 0x4D};
+
+    FILE* file = fopen(fileName, "rb");
+    if(file == NULL){
+        printf("Error: File not found");
+        return;
+    }
+
+
+    for (i = 0; i < 8; i++) {
+        byte = fgetc(file);
+        if (byte == EOF) {
+            printf("Error: File End\n");
+            fclose(file);
+            return;
+        }
+        fileStructure[i] = (unsigned char)byte;
+    }
+
+
+    fclose(file);
+
+    // Check PNG structure
+    int isPNG = 1;
+    for (i = 0; i < 8; i++) {
+        if (fileStructure[i] != pngStructure[i]) {
+            isPNG = 0;
+            break;
+        }
+    }
+
+    // Check GIF structure
+    int isGIF = 1;
+    for (i = 0; i < 6; i++) {
+        if (fileStructure[i] != gifStructure[i]) {
+            isGIF = 0;
+            break;
+        }
+    }
+
+    // Check BMP structure
+    int isBMP = 1;
+    for (i = 0; i < 2; i++) {
+        if (fileStructure[i] != bmpStructure[i]) {
+            isBMP = 0;
+            break;
+        }
+    }
+
+
+    if(isPNG){
+        printf("%s = PNG\n", fileName);
+    } else if (isGIF) {
+        printf("%s = GIF\n", fileName);
+    } else if (isBMP) {
+        printf("%s = BMP\n", fileName);
+    } else {
+        printf("%s Structure not found\n", fileName);
+    }
+
+    return;
+}
 
 void readFile(char* fileName){
 
@@ -41,9 +108,13 @@ void readFile(char* fileName){
 
 int main(){
 
-    char fileName[] = "task.png";
+    char filePng[] = "test.png";
+    char fileGif[] = "test.gif";
+    char fileBmp[] = "test.bmp";
 
-    readFile(fileName);
+    checkStructure(filePng);
+    checkStructure(fileGif);
+    checkStructure(fileBmp);
 
     return 0;
 }
